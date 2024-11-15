f214f1793b92f6689329d40573b3b6a31a25db82
Gunn-Wangwichit <93463941+Gunn-Wangwichit@users.noreply.github.com>
2024-11-15 17:55:00 +0000

    Fix some bugs and check testcases

--- test_zoo.py
+++ test_zoo.py
@@ -5,10 +5,21 @@ class TestZoo(unittest.TestCase):
     def setUp(self):
         self.zoo = Zoo()
 
-    def test_child_ticket_price(self):
-        self.assertEqual(self.zoo.get_ticket_price(5), 50)
-       
-    # Add your additional test cases here.
+    def testcase_invalid_age(self): 
+        self.assertEqual(self.zoo.get_ticket_price(-1), 'Incorrect Input')
+
+    def testcase_age_between_0_and_12(self):
+        self.assertEqual(self.zoo.get_ticket_price(4), 50)
+
+    def testcase_age_between_13_and_20(self):
+        self.assertEqual(self.zoo.get_ticket_price(19), 100)
+
+    def testcase_age_between_21_and_60(self):
+        self.assertEqual(self.zoo.get_ticket_price(24), 150)
+        self.assertEqual(self.zoo.get_ticket_price(45), 150)
+
+    def testcase_age_greater_than_60(self):
+        self.assertEqual(self.zoo.get_ticket_price(70), 100)
 
 if __name__ == '__main__':
     unittest.main()
